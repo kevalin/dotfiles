4b8ac6a58c2526837869dec4ebc2276ef75870bc
kevalin <kevalin123@gmail.com>
2023-11-16 18:07:58 +0800

    fix: filetype parse error

--- Neovim/after/filetype.lua
+++
@@ -1,7 +0,0 @@
--- 定义一个自动命令，当打开一个文件时，检查文件后缀并设置 filetype
-vim.api.nvim_exec([[
-  augroup filetypedetect
-    autocmd!
-    autocmd BufRead,BufNewFile *.hurl setfiletype hurl
-  augroup END
-]], false)
---
+++ Neovim/after/filetype/hurl.lua
@@ -0,0 +1,1 @@
+vim.cmd([[autocmd BufRead,BufNewFile *.hurl setfiletype hurl]])
